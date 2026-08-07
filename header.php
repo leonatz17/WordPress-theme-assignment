ae46ea8f63cceba46fdf663a46fe474033d82534
Leonife Natividad <natzyleon@gmail.com>
2026-08-07 18:42:32 +0800

    second commit

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <nav>
       <a href="#home">Home</a>
       <a href="#about">About</a>
-      <a href="#">Services</a>
-      <a href="#">Contact</a>
+      <a href="#services">Services</a>
+      <a href="#contact">Contact</a>
     </nav>
   </header>
