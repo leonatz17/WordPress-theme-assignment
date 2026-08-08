cc3b10626547eecf6a0f70b18b02b4dac1d07942
leonatz17 <natzyleon@gmail.com>
2026-08-08 16:04:46 +0800

    hjkl

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer class="footer">
   <p>&copy; 2026 Sip Shot Cafe. All Rights Reserved.</p>
 </footer>
+
 <?php wp_footer() ?>
 </body>
 </html>
---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<section id="about">
+
+        <div class="aboutUs">
+            <h1 class="home-header"><?php the_title(); ?></h1>
+            
+            <div class="story">
+                <div>
+                    <img id="cafe" src="<?php echo get_template_directory_uri(); ?>/assets/images/cafe.png" alt="">
+                </div>
+                <div id="story-box">
+                    <p id="title"><?php the_title(); ?> </p>
+                    <p id="title"> <?php the_content(); ?></p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
